0e624abdfa605119e93cb3442fc5f3667dea5f84
Muneeb Usmani <muneebusmani8355@gmail.com>
2024-05-29 00:19:28 +0500

    some more bullshitty changes

--- zsh/lazyload.zsh
+++ zsh/lazyload.zsh
@@ -1,7 +1,6 @@
-
 lazyload warp-cli -- '. <(warp-cli generate-completions zsh)'
 lazyload fuck -- '. <(thefuck --alias)'
 lazyload fk -- '. <(thefuck --alias fk)'
 lazyload z -- '. <(zoxide init zsh)'
-lazyload artisan-completion -- '. ~/functions/artisan-completion'
+lazyload artisan-completion -- '. <(./artisan completion)'
 lazyload nvm -- '. ~/.nvm/init-nvm.sh'
